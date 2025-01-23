a3859f550a8dca73d3ddfcc456a6caed872c0bdc
Aditya Tile <adityatile99@gmail.com>
2025-01-23 23:37:29 +0530

    implement home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
-import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import SigninButton from "@/components/SigninButton";
+import { getAuthSession } from "@/lib/nextauth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return <Button>
-    Hello World!
-  </Button>;
+export default async function Home() {
+  const session = await getAuthSession();
+  if (session?.user) {
+    redirect("/dashboard");
+  }
+  return (
+    <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+      <Card className="w-[300px]">
+        <CardHeader>
+          <CardTitle>Welcome To QuizeSense!</CardTitle>
+          <CardDescription>
+            QuizeSense is a platform for creating quizzes using AI!. Get started
+            by loggin in below!
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SigninButton text="Sign In with Google" />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
