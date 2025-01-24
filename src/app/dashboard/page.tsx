8e6919b0fb5ca5b723bb21ef8a927f6bcec6fe94
Aditya Tile <adityatile99@gmail.com>
2025-01-24 11:28:58 +0530

    complete quize card component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,32 @@
+import HistoryCard from "@/components/dashboard/HistoryCard";
+import QuizeSenseCard from "@/components/dashboard/QuizeSenseCard";
+import { getAuthSession } from "@/lib/nextauth";
+import { redirect } from "next/navigation";
 import React from "react";
 
 type Props = {};
 
-const Dashboard = (props: Props) => {
-  return <div>Dashboard</div>;
+export const metadata = {
+  title: "Dashboard | QuizeSense",
+};
+
+const Dashboard = async (props: Props) => {
+  const session = await getAuthSession();
+  if (!session?.user) {
+    return redirect("/");
+  }
+  return (
+    <main className="p-8 mx-auto max-w-7xl">
+      <div className="flex items-center">
+        <h2 className="mr-2 text-3xl font-bold tracking-tight">Dashboard</h2>
+      </div>
+      <div className="grid gap-4 mt-4 md:grid-cols-2">
+        <QuizeSenseCard />
+        <HistoryCard />
+      </div>
+      <div className="grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-7"></div>
+    </main>
+  );
 };
 
 export default Dashboard;
